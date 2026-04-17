b4394b6f1d565687583880cec3de486127285b63
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-04-17 08:31:28 -0500

    Add robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://marcas-y-patentes.vercel.app/sitemap.xml",
+  };
+}
